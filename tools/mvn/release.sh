7addfdec03e3973de897e499294aa534b31ec021
Jesper Pedersen <jesper.pedersen@redhat.com>
2011-05-30 10:57:28 -0400

    Use ivy:makepom for Maven artifacts

--- tools/mvn/release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-PWD=`pwd`
-VERSION=0.9.0.RC3
-mvn deploy:deploy-file -Dfile=../../target/fungal.jar -DpomFile=core.xml -Durl=file:$PWD/../../../www/fungal/maven2/
-mvn deploy:deploy-file -Dfile=../../target/fungal-sources.jar -DgeneratePom=false -DgroupId=com.github.fungal -DartifactId=fungal -Dversion=$VERSION -Dclassifier=sources -Dpackaging=jar -Durl=file:$PWD/../../../www/fungal/maven2/
-mvn deploy:deploy-file -Dfile=../../target/fungal-javadoc.jar -DgeneratePom=false -DgroupId=com.github.fungal -DartifactId=fungal -Dversion=$VERSION -Dclassifier=javadoc -Dpackaging=jar -Durl=file:$PWD/../../../www/fungal/maven2/
-mvn deploy:deploy-file -Dfile=../../target/fungal-cli.jar -DpomFile=cli.xml -Durl=file:$PWD/../../../www/fungal/maven2/
-mvn deploy:deploy-file -Dfile=../../target/fungal-cli-sources.jar -DgeneratePom=false -Dclassifier=sources -Dpackaging=jar -DgroupId=com.github.fungal -DartifactId=fungal-cli -Dversion=$VERSION -Durl=file:$PWD/../../../www/fungal/maven2/
