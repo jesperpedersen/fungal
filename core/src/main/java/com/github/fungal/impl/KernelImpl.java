8114fac22121d9335bf46c6f738ba459960ca588
Bob McWhirter <bob@mcwhirter.org>
2011-06-19 21:38:09 -0400

    Synchronize around beanLatches when performing comparison against other beans which may be asynchronously reporting and changing their state.  Thus, state-changing is also synchronized around the beanLatches collection.
    It seems sorta goofy now, though, to synchronize around a ConcurrentHashMap.
    Probably either need figure out some better synchronization primitive for
    this case, or continue forward, and use a regular HashMap, since we're providing
    external synchronization.

--- core/src/main/java/com/github/fungal/impl/KernelImpl.java
+++ core/src/main/java/com/github/fungal/impl/KernelImpl.java
@@ -929,26 +929,29 @@ public class KernelImpl implements Kernel, KernelImplMBean
    {
       beanStatus.put(name, status);
 
-      if (status == ServiceLifecycle.NOT_STARTED)
+      synchronized ( this.beanLatches ) 
       {
-         List<CountDownLatch> l = beanLatches.get(name);
-         if (l == null)
-         {
-            List<CountDownLatch> newList = Collections.synchronizedList(new ArrayList<CountDownLatch>(1));
-            beanLatches.putIfAbsent(name, newList);
-         }
-      }
-      else if (status == ServiceLifecycle.STARTED || status == ServiceLifecycle.ERROR)
-      {
-         List<CountDownLatch> l = beanLatches.get(name);
-         if (l != null)
-         {
-            for (CountDownLatch cdl : l)
-            {
-               if (cdl.getCount() > 0)
-                  cdl.countDown();
-            }
-         }
+          if (status == ServiceLifecycle.NOT_STARTED)
+          {
+             List<CountDownLatch> l = beanLatches.get(name);
+             if (l == null)
+             {
+                List<CountDownLatch> newList = Collections.synchronizedList(new ArrayList<CountDownLatch>(1));
+                beanLatches.putIfAbsent(name, newList);
+             }
+          }
+          else if (status == ServiceLifecycle.STARTED || status == ServiceLifecycle.ERROR)
+          {
+             List<CountDownLatch> l = beanLatches.get(name);
+             if (l != null)
+             {
+                for (CountDownLatch cdl : l)
+                {
+                   if (cdl.getCount() > 0)
+                      cdl.countDown();
+                }
+             }
+          }
       }
    }
 
@@ -1084,26 +1087,29 @@ public class KernelImpl implements Kernel, KernelImplMBean
       
       dependants.add(to);
 
-      // Register the count down latch if the 'to' hasn't started
-      ServiceLifecycle slc = getBeanStatus(to);
-      if (slc != ServiceLifecycle.STARTED && slc != ServiceLifecycle.ERROR)
+      synchronized ( this.beanLatches ) 
       {
-         List<CountDownLatch> l = beanLatches.get(to);
-         if (l == null)
-         {
-            List<CountDownLatch> newList = Collections.synchronizedList(new ArrayList<CountDownLatch>(1));
-            l = beanLatches.putIfAbsent(to, newList);
-            if (l == null)
-            {
-               l = newList;
-            }
-         }
-         
-         l.add(cdl);
-      }
-      else
-      {
-         cdl.countDown();
+          // Register the count down latch if the 'to' hasn't started
+          ServiceLifecycle slc = getBeanStatus(to);
+          if (slc != ServiceLifecycle.STARTED && slc != ServiceLifecycle.ERROR)
+          {
+             List<CountDownLatch> l = beanLatches.get(to);
+             if (l == null)
+             {
+                List<CountDownLatch> newList = Collections.synchronizedList(new ArrayList<CountDownLatch>(1));
+                l = beanLatches.putIfAbsent(to, newList);
+                if (l == null)
+                {
+                   l = newList;
+                }
+             }
+             
+             l.add(cdl);
+          }
+          else
+          {
+             cdl.countDown();
+          }
       }
    }
 
