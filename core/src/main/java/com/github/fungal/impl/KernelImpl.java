32547d86b55f4affc8322ee349507e46118ffbc7
Jesper Pedersen <jesper.pedersen@jboss.org>
2012-07-16 09:20:27 -0400

    Fungal 0.11.0.Beta12

--- core/src/main/java/com/github/fungal/impl/KernelImpl.java
+++ core/src/main/java/com/github/fungal/impl/KernelImpl.java
@@ -89,7 +89,7 @@ import javax.management.ObjectName;
 public class KernelImpl implements Kernel, KernelImplMBean
 {
    /** Version information */
-   private static final String VERSION = "Fungal 0.11.0.Beta11";
+   private static final String VERSION = "Fungal 0.11.0.Beta12";
 
    /** Kernel configuration */
    private KernelConfiguration kernelConfiguration;
